4e019cc18c29270ea01cd3fe8690333e16070047
oskar <oscardelicadohernandez@gmail.com>
2023-01-20 13:06:35 +0100

    Añadida función cargarConsolas (por terminar)

--- JS/generos2.js
+++ JS/generos2.js
@@ -1,18 +1,44 @@
-const cargarGeneros = async() =>{
-try{
+const cargarJuegos = async() =>{
+    try{
 
-const options =await fetch( 'https://api.rawg.io/api/games?key=d22b44fd751e438f943040e82cf43c0e',{
-    method:'GET'
-}
-);
+    const options = await fetch(`https://api.rawg.io/api/games?key=d22b44fd751e438f943040e82cf43c0e&page=${pagina}`,{
+        method:'GET'
+    }
+    );
 
-if(options.status===200){
-    const options2=await options.json();
-    console.log(options2);
+    if(options.status===200){
+        const options2=await options.json();
+        cargarConsolas(options2);
+        return options2;
+    }
+
+    }catch(error){
+        console.log(error);
+    }
 }
 
-}catch(error){
-    console.log(error);
+const cargarConsolas = async(platformName) =>{
+    try{
+
+    const options = await fetch('https://api.rawg.io/api/platforms?key=d22b44fd751e438f943040e82cf43c0e',{
+        method:'GET'
+    }
+    );
+
+    if(options.status===200){
+        const options2=await options.json();
+        for(let i = 0; i < options2.results.length; i++){
+            if(options2.results[i].name === platformName){
+                for(let j = 0; j < options2.results[i].games.length; j++){
+                    console.log(options2.results[i].games[j].name);
+                }
+            }
+        }
+    }
+
+    } catch(error){
+        console.log(error);
+    }
 }
-}
-cargarGeneros();
+
+cargarConsolas("Wii");
