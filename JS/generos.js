abda9e63075e2ae533ce1151185ddc760e90914b
oskar <oscardelicadohernandez@gmail.com>
2023-01-23 10:15:28 +0100

    Nueva función cargar juegos por consolas

--- JS/generos.js
+++ JS/generos.js
@@ -1,4 +1,32 @@
-const cargarConsolas = async(platformName) =>{
+const cargarJuegos = async(platformSelected) => {
+    try{
+
+        const options = await fetch('https://api.rawg.io/api/games?key=d22b44fd751e438f943040e82cf43c0e', {
+            method: 'GET'
+        }
+        );
+
+        if(options.status === 200){
+            const games = await options.json();
+
+            for(var i = 0; i < games.results.length; i++){
+                for(var j = 0; j < games.results[i].platforms.length; j++){
+                    if(games.results[i].platforms[j].platform.name === platformSelected){
+                        console.log(games.results[i].name);
+                    }
+                    
+                }
+            }
+        }
+
+    } catch(error){
+        console.log(error);
+    }
+}
+
+cargarJuegos("PC");
+
+/*const cargarConsolas = async(platformName) =>{
     try{
 
     const options = await fetch('https://api.rawg.io/api/platforms?key=d22b44fd751e438f943040e82cf43c0e',{
@@ -22,4 +50,4 @@ const cargarConsolas = async(platformName) =>{
     }
 }
 
-cargarConsolas("PlayStation 5");
+cargarConsolas("");*/
