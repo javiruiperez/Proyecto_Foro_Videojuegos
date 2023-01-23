e8ce20db2d89bff9ec22435ecd9da7ab98df5039
oskar <oscardelicadohernandez@gmail.com>
2023-01-23 13:57:31 +0100

    Nueva función arreglada cargarImagenes (comentada)

--- JS/generos.js
+++ JS/generos.js
@@ -60,28 +60,56 @@ eventPlatform.addEventListener("change", function(){
 
  /*MEJORAR PAGINACIÓN, QUE APAREZCA LOS JUEGOS DE CADA PÁGINA PERO EL USUARIO TIENE BOTÓN DE SIGUIENTE PÁGINA*/
 
-/*const cargarConsolas = async(platformName) =>{
-    try{
-
-    const options = await fetch('https://api.rawg.io/api/platforms?key=d22b44fd751e438f943040e82cf43c0e',{
-        method:'GET'
-    }
-    );
-
-    if(options.status===200){
-        const options2=await options.json();
-        for(let i = 0; i < options2.results.length; i++){
-            if(options2.results[i].name === platformName){
-                for(let j = 0; j < options2.results[i].games.length; j++){
-                    console.log(options2.results[i].games[j].name);
-                }
-            }
-        }
-    }
-
-    } catch(error){
-        console.log(error);
-    }
-}
-
-cargarConsolas("");*/
+ numeroPagina = 1;
+ totalJuegos = 0;
+ newGames = "";
+ 
+//  const cargarImagenesJuegosPorGenero = async(genreSelected) => {
+//      try{
+//          const options =await fetch( `https://api.rawg.io/api/games?key=d22b44fd751e438f943040e82cf43c0e&page=${numeroPagina}`,{
+//          method:'GET'
+//      }
+//      );
+ 
+//          if(options.status === 200){
+//              const games = await options.json();
+ 
+//              for(var i = 0; i < games.results.length; i++){
+//                  for(var j = 0; j < games.results[i].genres.length; j++){
+//                      if(games.results[i].genres[j].name === genreSelected){
+//                          if(totalJuegos >= 10){
+//                              break;
+//                          }
+//                          let name_game = games.results[i].name;
+//                          let image_game = games.results[i].background_image;
+//                          newGames += `<div class="responsive">
+//                          <div class="gallery">
+//                            <a target="_blank" href="${image_game}">
+//                              <img class="" src="${image_game}" width="400" height="250">
+//                              <p>${name_game}</p>
+//                            </a>
+//                          </div>
+//                          </div>`
+//                          totalJuegos++;
+//                      }
+//                  }
+//              }
+//              if(totalJuegos < 10){
+//                  numeroPagina++;
+//                  cargarImagenesJuegosPorGenero(genreSelected);
+//              } else{
+//                  document.getElementById("platform").innerHTML = newGames;
+//                  //We initialize these three variables in order to be able to load other platform later
+//                  totalJuegos = 0; 
+//                  newGames = "";
+//                  numeroPagina = 1;
+//              }
+ 
+//          }
+ 
+//      } catch(error){
+//          console.log(error);
+//      }
+//  }
+    
+//      cargarImagenesJuegosPorGenero("Action");
--- JS/generos2.js
+++ JS/generos2.js
@@ -1,57 +1,35 @@
 numeroPagina=1;
+const cargarGeneros = async(genero) =>{
+try{
 
+const options =await fetch( `https://api.rawg.io/api/games?key=d22b44fd751e438f943040e82cf43c0e&page=${numeroPagina}`,{
+    method:'GET'
+}
+);
 
+if(options.status===200){
+    const options2=await options.json();
+    // console.log(options2);
+    options2.results.forEach(juegos=>{
+        juegos.genres.forEach(nombregeneros=>{
+            if(nombregeneros.name.includes(genero)){
+                console.log(nombregeneros.id);
+                console.log(juegos.name);
+            }
+        console.log(options2);
+             }
+        )
+        
+      
+       
+    });
+}
 
-const cargarImagenesJuegosPorGenero = async(genero) =>{
-    try{
-    
-    const options =await fetch( `https://api.rawg.io/api/games?key=d22b44fd751e438f943040e82cf43c0e&page=${numeroPagina}`,{
-        method:'GET'
-    }
-    );
-    images="";
-    if(options.status===200){
-        const options2=await options.json();
-        // console.log(options2);
-        options2.results.forEach(juegos=>{
-            juegos.genres.forEach(nombregeneros=>{
-                if(nombregeneros.name.includes(genero)){
-                  
-                    console.log(juegos.background_image);
-images=images+`<div class="responsive">
-<div class="gallery">
-  <a target="_blank" href="${juegos.background_image}">
-    <img class="" src="${juegos.background_image}" width="400" height="250">
-    <p>${juegos.name}</p>
-  </a>
-</div>
-</div>`
-                   
-
-document.getElementById('Images').innerHTML=images;
-
-
-
-                    
-                }
-            
-                 }
-            )
-            
-          
-           
-        });
-    }
-    
-    }catch(error){
-        console.log(error);
-    }
-    }
-
-
-   
-    cargarImagenesJuegosPorGenero("Shooter");
-
+}catch(error){
+    console.log(error);
+}
+}
+cargarGeneros("Shooter");
 // btnAnterior.addEventListener('click',()=>{
 // 	if(pagina>1){
 // 	pagina--;
